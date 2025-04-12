beba8cd497d11e10fb4d2397e092196de4ae5987
arkoroy05 <132930851+arkoroy05@users.noreply.github.com>
2025-04-12 11:44:30 +0530

    create token functionality

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -1,4 +1,51 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.27;
+import {Token} from "./Token.sol";
 
-contract Factory {}
+contract Factory {
+    uint256 public immutable fee;
+    address public owner;
+    address[] public tokens;
+    uint256 public totalTokens;
+    mapping(address=>TokenSale) public tokenSales;
+
+    event Created(address token);
+
+    struct TokenSale{
+        address token;
+        string name;
+        string symbol;
+        uint256 totalSupply;
+        uint256 sold;
+        uint256 raised;
+        bool isOpen;
+
+
+    }
+    constructor(uint256 _fee) {
+        fee = _fee;
+        owner = msg.sender;
+    }
+
+    function create(string memory _name,string memory _symbol,uint256 totalSupply) external payable{
+        require(msg.value>=fee,"Not enough fee sent");
+        //create a new token
+        Token token=new Token(msg.sender,_name,_symbol,totalSupply);
+        //storing the token address
+        tokens.push(address(token));
+        totalTokens++;
+        TokenSale memory sale=TokenSale(
+            address(token),
+            _name,
+            _symbol,
+            totalSupply,
+            0,
+            0,
+            true
+        );
+        tokenSales[address(token)]=sale;
+        emit Created(address(token));
+    }
+
+    
+}
--- test/Factory.js
+++ test/Factory.js
@@ -3,5 +3,13 @@ const { expect } = require("chai")
 const { ethers } = require("hardhat")
 
 describe("Factory", function () {
-
+    it("check the name",async function(){
+        //hardhat tests are blank by default, so we need to fetch the contract
+        const Factory=await ethers.getContractFactory("Factory")
+        //we need to mock deploy
+        const factory=await Factory.deploy()
+        //check the name
+        const name=await factory.name()
+        expect(name).to.equal("Factory")
+    })
 })
