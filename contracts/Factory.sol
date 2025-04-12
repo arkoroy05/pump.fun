c8c6eadc4e7cca4d38d2db2957c8f119ef6b8cdf
arkoroy05 <132930851+arkoroy05@users.noreply.github.com>
2025-04-12 12:12:58 +0530

    SC complted

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -1,65 +1,136 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.27;
+
 import {Token} from "./Token.sol";
 
+import "hardhat/console.sol";
+
 contract Factory {
+    uint256 public constant TARGET = 3 ether;
+    uint256 public constant TOKEN_LIMIT = 500_000 ether;
     uint256 public immutable fee;
     address public owner;
-    address[] public tokens;
+
     uint256 public totalTokens;
-    mapping(address=>TokenSale) public tokenSales;
+    address[] public tokens;
+    mapping(address => TokenSale) public tokenToSale;
 
-    event Created(address token);
-    event Buy(address token, uint256 amount);
-
-    struct TokenSale{
+    struct TokenSale {
         address token;
         string name;
-        string symbol;
-        uint256 totalSupply;
+        address creator;
         uint256 sold;
         uint256 raised;
         bool isOpen;
-
-
     }
+
+    event Created(address indexed token);
+    event Buy(address indexed token, uint256 amount);
+
     constructor(uint256 _fee) {
         fee = _fee;
         owner = msg.sender;
     }
 
-    function create(string memory _name,string memory _symbol,uint256 totalSupply) external payable{
-        require(msg.value>=fee,"Not enough fee sent");
-        //create a new token
-        Token token=new Token(msg.sender,_name,_symbol,totalSupply);
-        //storing the token address
+    function getTokenSale(
+        uint256 _index
+    ) public view returns (TokenSale memory) {
+        return tokenToSale[tokens[_index]];
+    }
+
+    function getCost(uint256 _sold) public pure returns (uint256) {
+        uint256 floor = 0.0001 ether;
+        uint256 step = 0.0001 ether;
+        uint256 increment = 10000 ether;
+
+        uint256 cost = (step * (_sold / increment)) + floor;
+        return cost;
+    }
+
+    function create(
+        string memory _name,
+        string memory _symbol
+    ) external payable {
+        require(msg.value >= fee, "Factory: Creator fee not met");
+
+        Token token = new Token(msg.sender, _name, _symbol, 1_000_000 ether);
+
+        // Store token address
         tokens.push(address(token));
+
+        // Increment total tokens
         totalTokens++;
-        TokenSale memory sale=TokenSale(
+
+        // Create the sale.
+        TokenSale memory sale = TokenSale(
             address(token),
             _name,
-            _symbol,
-            totalSupply,
+            msg.sender,
             0,
             0,
             true
         );
-        tokenSales[address(token)]=sale;
+
+        // Save the sale to mapping.
+        tokenToSale[address(token)] = sale;
+
         emit Created(address(token));
     }
 
-    function buy(address _token,uint amount) external payable{
-        TokenSale storage sale=tokenSales[_token]; //fetches you the struct, and puts inside the sale variable
-        //conditions to check if the sale is open
-        require(sale.isOpen,"Sale is closed");
-        require(sale.totalSupply>=sale.sold+amount,"Not enough tokens left");
+    function buy(address _token, uint256 _amount) external payable {
+        TokenSale storage sale = tokenToSale[_token];
 
-        //upddate the sale struct
-        sale.sold+=amount;
-        sale.raised+=msg.value;
-        //transfer the tokens to the buyer
-        Token(_token).transfer(msg.sender,amount);
-        
-        emit Buy(_token,amount);
+        require(sale.isOpen == true, "Factory: Buying closed");
+        require(_amount >= 1 ether, "Factory: Amount too low");
+        require(_amount <= 10000 ether, "Factory: Amount exceeded");
+
+        // Calculate the price of 1 token based on the total bought.
+        uint256 cost = getCost(sale.sold);
+
+        // Determine the total price for X amount.
+        uint256 price = cost * (_amount / 10 ** 18);
+
+        // Check to ensure enough ETH is sent.
+        require(msg.value >= price, "Factory: Insufficient ETH received");
+
+        // Update contract states.
+        sale.sold += _amount;
+        sale.raised += price;
+
+        // If we have reached our ETH goal OR buy limit, stop allowing buys.
+        if (sale.sold >= TOKEN_LIMIT || sale.raised >= TARGET) {
+            sale.isOpen = false;
+        }
+
+        // Transfer tokens to buyer.
+        Token(_token).transfer(msg.sender, _amount);
+
+        emit Buy(_token, _amount);
+    }
+
+    function deposit(address _token) external {
+        // The remaining token balance and the ETH raised
+        // would go into a liquidity pool like Uniswap V3.
+        // For simplicity we'll just transfer remaining
+        // tokens and ETH raised to the creator.
+
+        Token token = Token(_token);
+        TokenSale memory sale = tokenToSale[_token];
+
+        require(sale.isOpen == false, "Factory: Target not reached");
+
+        // Transfer tokens
+        token.transfer(sale.creator, token.balanceOf(address(this)));
+
+        // Transfer ETH raised
+        (bool success, ) = payable(sale.creator).call{value: sale.raised}("");
+        require(success, "Factory: ETH transfer failed");
+    }
+
+    function withdraw(uint256 _amount) external {
+        require(msg.sender == owner, "Factory: Not owner");
+
+        (bool success, ) = payable(owner).call{value: _amount}("");
+        require(success, "Factory: ETH transfer failed");
     }
 }
